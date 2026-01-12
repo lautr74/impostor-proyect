93638f88de522cb31640a004b6ec322ffef1e364
lautaro <lautaro.escales.98@gmail.com>
2026-01-12 19:25:50 +0100

    vite.config

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['lautaro.space', 'www.lautaro.space'],
     port: 5173,
+    host: true
   }
 })
